8ef3a22f8bc74abe8d93c4d1db9d52de71213e68
raihanuldev <rihanthedev@gmail.com>
2024-09-24 15:41:26 +0600

    succesfully runed

---
+++ frist.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello World");
+    printf("hello bondo dhekeo ami C programming Shiktesi! ");
+    return 0;
+}
---
+++ output.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("This is Practice of Output get ");
+    printf("\n Hello Bondo Amio ekn Pro Hote jacci C Programming e . ");
+    printf("\n Inshallah Ami ekdin Google & Facebook and Brain Station e Job Korbo!");
+}
