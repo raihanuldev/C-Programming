ccd246de2720f71e54b7ceb427e35ba887dfad5b
raihanuldev <rihanthedev@gmail.com>
2024-09-24 15:50:16 +0600

    added

---
+++ varibale.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int myNum = 14;
+    float myFlote = 12.23;
+    char myName = "Raihan sharif";
+    printf("%d\n",myNum);
+    printf("%f\n",myFlote);
+    printf("%c\n",myName);
+}
