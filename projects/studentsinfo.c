6cefb7fccd9127431d1821c3367559c35fa83be0
raihanuldev <rihanthedev@gmail.com>
2024-09-27 18:32:28 +0600

    projects variable and data type

---
+++ projects/studentsinfo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int studentID = 14;
+    int studentAge = 23;
+    float studentFee = 75.65;
+    char studentGrade = 'f';
+
+    // print all info
+    printf("Thank you so much your interest here is all information added");
+    printf("Student ID: %d\n",studentID);
+    printf("Student Age: %d\n", studentAge);
+    printf("student Fee: %f\n", studentFee );
+    printf("Student Grade: %c\n", studentGrade);
+}
