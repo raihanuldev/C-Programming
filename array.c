72a5c0d330b8c84f8934d4fb556afc505946cac0
raihanuldev <rihanthedev@gmail.com>
2024-09-27 18:42:58 +0600

    arrays

---
+++ array.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int roll_numbers[] ={12,133,4232,432123,3442};
+    printf("Frist ELement of Array: %d\n",roll_numbers[0]);
+    printf("all elements of array: %d\n",roll_numbers);
+}
+
+
+// Todo: Fundamentals concepts of array. 
