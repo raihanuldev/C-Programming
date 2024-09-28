731c0fdac920799593b239546403eab88d11bcc1
raihanuldev <rihanthedev@gmail.com>
2024-09-29 01:54:12 +0600

    videos

---
+++ module1/frist.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int age = 20;
+    printf("hello world");
+    printf("\n%d",age);
+    return 0;
+}
+
+/*
+integer => 1232
+floate => 1.21
+bol => true /false
+char => a
+*/ 
