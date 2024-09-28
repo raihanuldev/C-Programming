32b29a4fef408d587ae488037909dd8086e884c3
raihanuldev <rihanthedev@gmail.com>
2024-09-28 22:23:15 +0600

    1

---
+++ basic_structure.c
@@ -0,0 +1,18 @@
+// this is commenting
+// this is linking section
+#include <stdio.h> //standrad input output
+#include <math.h> // for mathmatice operations
+
+// defination section
+#define PI 3.1416
+#define ll long long
+
+// global decalretoin
+int age = 12;
+int sum(int x,int y);
+
+// main function section
+int main(){
+    printf("Hello world");
+    return 0;
+}
