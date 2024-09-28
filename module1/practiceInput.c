a0c17c6be6d60af778d74d7900848dfafe23e264
raihanuldev <rihanthedev@gmail.com>
2024-09-28 23:47:22 +0600

    mutiple inputs

--- module1/practiceInput.c
+++ module1/practiceInput.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
 int main(){
-    int number;
-    scanf("%d", &number);
-    printf("%d",number);
+    int a,b,c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d %d %d",a,b,c);
 }
+
+
+
+/*
+
+Formate Specifier
+
+short ini %hd
+int %d
+long int %ld
+long long int %lld
+char  %c
+floate %f
+double %lf
+long double %Lf
+
+unassigned int  %u
+unassigned short %hu
+
+*/
