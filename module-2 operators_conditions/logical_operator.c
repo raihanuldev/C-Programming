f74f159a82ef3e6d9c3a57b0dfec851231742f86
raihanuldev <rihanthedev@gmail.com>
2024-09-30 07:56:43 +0600

    logical operators

--- module-2 operators_conditions/logical_operator.c
+++ module-2 operators_conditions/logical_operator.c
@@ -2,7 +2,8 @@
 #include <stdbool.h>
 
 int main(){
-    bool result = !((4>3) && (4<7));
+    bool result = !((4>3) && (4<7) || (8<3));
+    // ! sing means whole result convert. 
     printf("%d", result);
     return 0;
 }
