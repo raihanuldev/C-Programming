e31a10c7e76d8584253bfae9fd0a2c34bf6ec1ba
raihanuldev <rihanthedev@gmail.com>
2024-09-30 22:31:25 +0600

    contest

---
+++ practiceDay1/add_contest.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    int number;
+    scanf("%d", &number);
+    number+=5;
+    printf("%d",number);
+}
---
+++ practiceDay1/zero_contest.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int number;
+    scanf("%d", &number);
+
+    if(number==0){
+        printf("Zero");
+    }
+    else if(number <=-1){
+        printf("Non Zero");
+    }
+    else{
+        printf("Non Zero");
+    }
+}
