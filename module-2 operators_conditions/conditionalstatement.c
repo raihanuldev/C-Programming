10aafe551fa3c65e42373ac19f7aed317315c0b5
raihanuldev <rihanthedev@gmail.com>
2024-09-30 08:14:42 +0600

    if elase nested if else

--- module-2 operators_conditions/conditionalstatement.c
+++ module-2 operators_conditions/conditionalstatement.c
@@ -2,10 +2,22 @@
 
 int main(){
     // 
-    int a = 5;
-    printf("Before Condition \n");
-
-    if(a > 3){
-        printf("A is greater than 3");
+    int balance = 50;
+    int egg_price = 30;
+    int coffee = 20;
+    int remain;
+    if(egg_price <= balance){
+        printf("Egg Nia asis \n");
+        remain = balance-egg_price;
+        if(remain <= coffee){
+            printf("Please bring a coffee for me\n");
+            remain = 0;
+        }
+        else{
+            printf("Baki taka ghula tui kichu kais bhai! ");
+        }
+    }
+    else{
+        printf("I don't have enogh money for buy Egg. Sorry iam Goribs");
     }
 }
