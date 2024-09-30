d7d7a9126146e5e2c5d10c09ed93f19fbc434497
raihanuldev <rihanthedev@gmail.com>
2024-09-30 08:28:03 +0600

    CGP Maintain

---
+++ module-2 operators_conditions/ladder.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int CGP = 3.80;
+
+    if(CGP >= 4.00){
+        printf("Congrat you are got A+");
+    }
+    else if (CGP >=3.50){
+        printf("You are got A");
+    }
+    else if(CGP >= 3.00){
+        printf("you are got B");
+    }
+    else{
+        printf("you are fail in my syestem");
+    }
+
+}
