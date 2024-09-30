870bc39ae63b45bb423ccc3d7ed97109e9924179
raihanuldev <rihanthedev@gmail.com>
2024-10-01 00:25:39 +0600

    Frist added

---
+++ problem_solveing/frist.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    char s[500];
+    // printf("enter your text: ");
+    scanf("%s", &s);
+    printf("%s\n", s);
+}
+
+// TODO: You need to make sure catch all words like Hello, world . 
