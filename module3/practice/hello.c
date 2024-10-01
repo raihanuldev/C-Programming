704b46ccfc60ee71c57649badfe68b95bcaeb0c5
raihanuldev <rihanthedev@gmail.com>
2024-10-02 01:25:07 +0600

    add

--- module3/practice/hello.c
+++ module3/practice/hello.c
@@ -5,9 +5,22 @@ int main(){
     // for(int i = 1; i<100; i++){
     //     printf("I Love You❤️ %d\n",i);
     // }
-    int i =0;
-    while(i<5){
-        printf("Hello\n");
+
+    // while Loop
+    // int i =0;
+    // while(i<5){
+    //     printf("Hello\n");
+    //     i++;
+    // }
+
+    // Do While Loop;
+    int i = 0;
+    do
+    {
+        /* code */
+        printf("This is From Do while Loop\n");
         i++;
-    }
+    } while (i<5);
+    
+
 }
