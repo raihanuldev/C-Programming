85199617f025dfa91965cc71321b1599de0ef157
raihanuldev <rihanthedev@gmail.com>
2024-10-02 01:22:47 +0600

    while loop

--- module3/practice/hello.c
+++ module3/practice/hello.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
 int main(){
-    // for loop
-    for(int i = 1; i<100; i++){
-        printf("I Love You❤️ %d\n",i);
+    // // for loop
+    // for(int i = 1; i<100; i++){
+    //     printf("I Love You❤️ %d\n",i);
+    // }
+    int i =0;
+    while(i<5){
+        printf("Hello\n");
+        i++;
     }
 }
