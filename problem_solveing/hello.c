1384f66d2c44803d7562a43cb92055cf01b71587
raihanuldev <rihanthedev@gmail.com>
2024-10-02 20:54:45 +0600

    addfed

--- problem_solveing/hello.c
+++ problem_solveing/hello.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    int A,B;
-    scanf("%d %d", &A,&B);
-    printf("%d", A+B);
+    int A;
+    long long int B;
+    float C;
+    char D;
+    scanf("%d\n",&A);
+    scanf("%lld\n",&B);
+    scanf("%f\n",&C);
+    scanf("%c\n",&D);
+    
+    printf("%d\n",A);
+    printf("%lld\n",B);
+    printf("%.2f\n",C);
+    printf("%c\n",D);
     return 0;
 }
