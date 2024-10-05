3dd924e6eb1ed8dbaca6425c016fae950feb4488
raihanuldev <rihanthedev@gmail.com>
2024-10-06 01:58:04 +0600

    quiz

--- module4/quiz.c
+++ module4/quiz.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 
 int main()
+
 {
+    int x = 4;
+    if (x % 2 != 0)
     {
-        int x = 1;
-        if (x < 5)
-        {
-            printf("hello");
-        }
-        if (x <= 10)
-        {
-            printf("hi");
-        }
-        else
-        {
-            printf("no");
-        }
-        return 0;
+        printf("Even ");
+    }
+    else
+    {
+        printf("Odd ");
+    }
+    if (x > 0)
+    {
+        printf("Positive ");
+    }
+    else
+    {
+        printf("Negative ");
     }
 }
