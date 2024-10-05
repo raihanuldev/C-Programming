c7f58d51ea56b73c4f71066ee863df5225a4c4ab
raihanuldev <rihanthedev@gmail.com>
2024-10-06 01:29:52 +0600

    wrong at testcase 5

--- module4/maxMin.c
+++ module4/maxMin.c
@@ -4,51 +4,50 @@ int main()
 {
     int A, B, C;
     scanf("%d %d %d", &A, &B, &C);
-
-    if (A > B && A > C)
+    if (A < B && A < C)
     {
         printf("%d", A);
-        if (A < B && A < C)
+        if (A > B && A > C)
         {
             printf(" %d", A);
         }
-        else if (B < A && B < C)
+        else if (B > A && B > C)
         {
             printf(" %d", B);
         }
-        else if (C < A && C < B)
+        else if (C > A && C > B)
         {
             printf(" %d", C);
         }
     }
-    else if (B > A && B > C)
+    else if (B < A && B < C)
     {
         printf("%d", B);
-        if (A < B && A < C)
+        if (A > B && A > C)
         {
             printf(" %d", A);
         }
-        else if (B < A && B < C)
+        else if (B > A && B > C)
         {
             printf(" %d", B);
         }
-        else if (C < A && C < B)
+        else if (C > A && C > B)
         {
             printf(" %d", C);
         }
     }
-    else if (C > A && C > B)
+    else if (C < A && C < B)
     {
         printf("%d", C);
-        if (A < B && A < C)
+        if (A > B && A > C)
         {
             printf(" %d", A);
         }
-        else if (B < A && B < C)
+        else if (B > A && B > C)
         {
             printf(" %d", B);
         }
-        else if (C < A && C < B)
+        else if (C > A && C > B)
         {
             printf(" %d", C);
         }
