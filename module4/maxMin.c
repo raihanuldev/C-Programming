ab02b065d0304ccc07594c1bf5898f7d14a2c7ed
raihanuldev <rihanthedev@gmail.com>
2024-10-06 01:21:43 +0600

    ADDED

---
+++ module4/maxMin.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main()
+{
+    int A, B, C;
+    scanf("%d %d %d", &A, &B, &C);
+
+    if (A > B && A > C)
+    {
+        printf("%d", A);
+        if (A < B && A < C)
+        {
+            printf(" %d", A);
+        }
+        else if (B < A && B < C)
+        {
+            printf(" %d", B);
+        }
+        else if (C < A && C < B)
+        {
+            printf(" %d", C);
+        }
+    }
+    else if (B > A && B > C)
+    {
+        printf("%d", B);
+        if (A < B && A < C)
+        {
+            printf(" %d", A);
+        }
+        else if (B < A && B < C)
+        {
+            printf(" %d", B);
+        }
+        else if (C < A && C < B)
+        {
+            printf(" %d", C);
+        }
+    }
+    else if (C > A && C > B)
+    {
+        printf("%d", C);
+        if (A < B && A < C)
+        {
+            printf(" %d", A);
+        }
+        else if (B < A && B < C)
+        {
+            printf(" %d", B);
+        }
+        else if (C < A && C < B)
+        {
+            printf(" %d", C);
+        }
+    }
+}
