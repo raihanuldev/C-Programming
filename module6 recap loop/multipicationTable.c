1bd64b2c256df718bc9cd0e866cd2e28a44daf0d
raihanuldev <rihanthedev@gmail.com>
2024-10-07 03:52:11 +0600

    add

--- module6 recap loop/multipicationTable.c
+++ module6 recap loop/multipicationTable.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    int n=3;
+    int n = 3;
     for (int i = 1; i <= 10; i++)
     {
-        printf("%d x %2d = %d\n",n,i,n*i);
+        for (int j = 1; j <= 10; j++)
+        {
+            printf("%d x %2d = %d\n", i, j, j * i);
+        }
+        printf("new Mission started\n\n");
     }
 }
