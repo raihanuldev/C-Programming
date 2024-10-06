3e65329e75466b164bedbfd05535329eac9c11a4
raihanuldev <rihanthedev@gmail.com>
2024-10-07 03:22:07 +0600

    loop nested

---
+++ module6 recap loop/loop1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    for(int i =1; i<4;i++){
+        // printf("Hello world\n");
+        for(int j=1; j<9; j++){
+            printf("%d", j);
+        }
+        printf(" \n");
+    }
+}
