cc25e06e16a3f1fd46add41eda59ccec397ecc01
raihanuldev <rihanthedev@gmail.com>
2024-10-07 18:54:26 +0600

    added

--- module6 recap loop/digits.c
+++ module6 recap loop/digits.c
@@ -5,6 +5,9 @@ int main(){
     for(int i=0; i<t;i++){
         int n;
         scanf("%d",&n);
+        if(n==0){
+            printf("0");
+        }
         while(n>0){
             int digit = n%10;
             printf("%d ",digit);
