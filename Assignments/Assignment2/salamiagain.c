7bc474e8affb0f154b29ffcdaae53c349da9a552
raihanuldev <rihanthedev@gmail.com>
2024-10-14 17:42:29 +0600

    accpeted

--- Assignments/Assignment2/salamiagain.c
+++ Assignments/Assignment2/salamiagain.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     int salami[n];
-    for(int i=0; i<n;i++){
-        scanf("%d",&salami[i]);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &salami[i]);
     }
-
-    for(int i=0; i<n;i++){
+    // max value
+    int max = salami[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (max < salami[i])
+        {
+            max = salami[i];
+        }
+        // printf("%d \n", salami[i]);
+    }
+    for(int i =0; i<n; i++){
+        salami[i] = max - salami[i];
+    }
+    printf("%d ", max);
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", salami[i]);
     }
-
     return 0;
 }
