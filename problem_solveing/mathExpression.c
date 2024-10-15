964184f7038100f522077f7c354ea4cf26b19869
raihanuldev <rihanthedev@gmail.com>
2024-10-16 01:06:22 +0600

    mathExpression

---
+++ problem_solveing/mathExpression.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+    int a,b,c;
+    char s,q;
+    scanf("%d %c %d %c %d", &a, &s, &b, &q, &c);
+
+    if(s =='+'){
+        if(a+b==c){
+            printf("Yes");
+        }else{
+            printf("%d",a+b);
+        }
+        return 0;
+    }
+    if(s =='-'){
+        if(a-b==c){
+            printf("Yes");
+        }else{
+            printf("%d",a-b);
+        }
+        return 0;
+    }
+    if(s =='*'){
+        if(a*b==c){
+            printf("Yes");
+        }else{
+            printf("%d",a*b);
+        }
+        return 0;
+    }
+
+    return 0;
+}
