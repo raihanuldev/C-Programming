b98639e596ff243496f214330badf73047e08fc0
raihanuldev <rihanthedev@gmail.com>
2024-10-17 02:52:27 +0600

    summation

---
+++ problem_solveing/Summation.c
@@ -0,0 +1,12 @@
+    #include <stdio.h>
+
+    int main(){
+        int n;
+        scanf("%d",&n);
+        long long  sum = (1LL *(n+1)*n)/2;
+
+        
+        printf("%lld",sum);
+
+        return 0;
+    }
