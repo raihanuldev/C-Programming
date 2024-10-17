5234091239d904b75616569588a7bdccaeebc9e6
raihanuldev <rihanthedev@gmail.com>
2024-10-17 15:24:33 +0600

    q

---
+++ problem_solveing/pattern.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+
+    for(int i=1; i<=n;i+2){
+        printf("%d\n",i);
+    }
+    return 0;
+}
+
+
+
+
+
+
+/*
+   *         1
+  ***        3
+ *****       5
+*******      7
+*******      7
+ *****       5
+  ***        3
+   *         1
+
+*/
