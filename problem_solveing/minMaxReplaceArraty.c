c17f18cd2ef8158b628eb69d700c8eb357e0a337
raihanuldev <rihanthedev@gmail.com>
2024-10-18 21:36:28 +0600

    minMaxReplace

--- problem_solveing/minMaxReplaceArraty.c
+++ problem_solveing/minMaxReplaceArraty.c
@@ -10,17 +10,32 @@ int main()
     {
         scanf("%d", &a[i]);
     }
-    int min=a[0];
-    int max = a[1];
+    int min = a[0];
+    int max = a[0];
+    int min_pos = 0, max_pos = 0;
+    for (int i = 0; i < n; i++)
+    {
 
-    for(int i=0; i<n;i++){
-        if(min>a[i]){
+        if (a[i] < min)
+        {
             min = a[i];
+            min_pos = i;
         }
-        if(max>a[i]){
-            max =a[i];
+        if (a[i] > max)
+        {
+            max = a[i];
+            max_pos = i;
         }
     }
-    printf("%d %d", min,max);
+    // int temp = min;
+    // min = max;
+    // max = temp;
+    a[min_pos] = max;
+    a[max_pos] = min;
+    // printf("%d %d", min, max);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
     return 0;
 }
