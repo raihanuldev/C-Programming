88a45e3cae81348aa7980427c9ae923c117a938f
raihanuldev <rihanthedev@gmail.com>
2024-10-22 03:05:33 +0600

    all added

---
+++ Practice-Problem/Week4/ariaOfCircle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    double r;
+    scanf("%lf", &r);
+
+    double pi = 3.141592653;
+    double area = pi *(r*r);
+
+    printf("%.9lf\n", area);
+
+    return 0;
+}
+
+//eitao problem
+//https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/E
