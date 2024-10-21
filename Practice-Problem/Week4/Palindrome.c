d0427e864550cacc007cbb45f1d028cc452be3ff
raihanuldev <rihanthedev@gmail.com>
2024-10-22 03:35:23 +0600

    aaaaaaaaaaaaa

---
+++ Practice-Problem/Week4/Palindrome.c
@@ -0,0 +1,19 @@
+// #include <stdio.h>
+
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     int count =0;
+
+//     do{
+//         n/=10;
+//         ++count;
+//     }while(n!=0);
+//     // printf("%d",count);
+//     int New[count];
+//     for(int i =0; i < count; i++){
+//         new[i] = n[i]
+//     }
+
+//     return 0;
+// }
