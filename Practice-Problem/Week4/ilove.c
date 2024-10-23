1fce92d1822ba20d94454c18a5308f3eafbcd69f
raihanuldev <rihanthedev@gmail.com>
2024-10-24 01:04:56 +0600

    core logic added

--- Practice-Problem/Week4/ilove.c
+++ Practice-Problem/Week4/ilove.c
@@ -1,18 +1,46 @@
 #include <stdio.h>
 #include <string.h>
 
-
-int main(){
+int main()
+{
     // int n;
     // scanf("%d",&n);
-    char s[51],t[51];
-    scanf("%s %s",s,t);
+    char s[51], t[51];
+    scanf("%s %s", s, t);
 
     int sLen = strlen(s);
     int tLen = strlen(t);
 
-    for(int i =0; s[i]!='\0'; i++){
-        printf("%c%c",s[i],t[i]);
+    if (sLen >= tLen)
+    {
+        // jodi s size besi hoi.
+        for (int i = 0; s[i] != '\0'; i++)
+        {
+            if (t[i] != '\0')
+            {
+                printf("%c%c", s[i], t[i]);
+            }
+            else
+            {
+                printf("%c", s[i]);
+            }
+        }
     }
+    else
+    {
+        // t len jodi bodo hoi tahole eita hobe.
+        for (int i = 0; t[i] != '\0'; i++)
+        {
+            if (s[i] != '\0')
+            {
+                printf("%c%c", s[i], t[i]);
+            }
+            else
+            {
+                printf("%c", t[i]);
+            }
+        }
+    }
+
     return 0;
 }
