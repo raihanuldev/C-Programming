ff5f3643755246800ba6f825803d325085907e30
raihanuldev <rihanthedev@gmail.com>
2024-10-25 08:10:04 +0600

    add

--- Assignments/MidExam.C/compare.c
+++ Assignments/MidExam.C/compare.c
@@ -3,20 +3,30 @@
 
 int main(){
     
-    char s1[1001];
-    char s2[1001];
-    char s3[1001];
+    char s1[1001], s2[1001], s3[1001];
+    char min[1001], max[1001];
 
-    scanf("%s %s %s", s1,s2,s3);
+    scanf("%s %s %s", s1, s2, s3);
 
-    char min[1001] = s1;
-    char max[1001] = s1;
+    strcpy(min, s1);
+    strcpy(max, s1);
 
-    printf("%d",strcmp(s3,s2));
+    if (strcmp(s2, min) < 0) {
+        strcpy(min, s2);  
+    }
+    if (strcmp(s2, max) > 0) {
+        strcpy(max, s2); 
+    }
 
+    if (strcmp(s3, min) < 0) {
+        strcpy(min, s3);  
+    }
+    if (strcmp(s3, max) > 0) {
+        strcpy(max, s3);  
+    }
 
-
-    printf("%s",s2);
+    printf("%s\n", min);
+    printf("%s\n", max);
 
     return 0;
 }
