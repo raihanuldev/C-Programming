17e088f494e09857a16106f872408d34ead577fc
raihanuldev <rihanthedev@gmail.com>
2024-10-27 06:50:36 +0600

    aaaaaaaaa

--- module13/FrequenceArray.c
+++ module13/FrequenceArray.c
@@ -3,19 +3,19 @@
 int main(){
     int n;
     scanf("%d",&n);
-    int a[n] = {0};
+    int a[1000] = {0};
 
     for(int i = 0; i< n; i++){
         scanf("%d",&a[i]);
     }
     // 
-    // int b[100000];
-    // for(int i = 0; i<n; i++){
-    //     b[a[i]] =1;
-    // }
-
+    int b[100000];
+    for(int i = 0; i<n; i++){
+        b[a[i]] =1;
+    }
+//Frequence array
     for(int i =0; i<10; i++){
-        printf("%d ", a[i]);
+        printf("%d frist \n", b[i]);
     }
 
     return 0;
