f6ac2711f88123ff5360020d092a9dadfee00b69
raihanuldev <rihanthedev@gmail.com>
2024-11-01 15:54:55 +0600

    accpted

--- problem_solveing/difficulty800/ab.c
+++ problem_solveing/difficulty800/ab.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main(){
     int n;
     scanf("%d",&n);
 
     for(int i =0; i<n; i++){
-        int inputValue;
-        scanf("%d",&inputValue);
-        int lastvalue = inputValue%10;
-        int frist = inputValue/10;
-        printf("%d\n",frist+lastvalue);
+        int frist;
+        int secound;
+        scanf("%d",&frist);
+        scanf("%d",&secound);
+        int result = frist-secound;
+        printf("%d\n",abs(result));
 
     }
 
